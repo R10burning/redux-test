c3332156b80b7610274d1035e1e50839df3f1cc9
dengxiang <790309793@qq.com>
2018-05-14 11:22:22 +0800

    123

---
+++ src/test.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Test extends Component {
+  render() {
+    return (
+      <div style={styles.test} className="213">
+      123
+      </div>
+    );
+  }
+}
+
+const styles = {
+    test:{
+        color:'red'
+    }
+}
+
+export default Test;
